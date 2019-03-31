a2ea01c8aa3786a7241146c4d2c0e075d001b054
Duncan <frsdun@gmail.com>
2019-03-31 17:24:14 +0200

    blog basics

---
+++ docs/blogs/fish/day 1.md
@@ -0,0 +1,10 @@
+# Day 1
+
+Here is a paragraph
+about the time we went
+hiking in Nambia
+
+<div class="gallery">
+<img src="./img/fish/fish_landscape.jpg" alt="my image">
+<img src="./img/fish/fish_portrait.jpg" alt="my image">
+</div>
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,27 +1,34 @@
 "use strict";
 
 // Find all 'js-insert' tags and insert their markdown
-(function () {
-    $(document).ready(function () {
-        $('.js-insert').each(function (index, element) {
-            var url = element.getAttribute('insert-url');
-            if (url) {
-                console.log(url)
-                try {
-                    $.get(url, function (data) {
-                        if (url.endsWith('.md')) {
-                            element.innerHTML = marked(data, { 'sanitize': true });
-                        }
-                        if (url.endsWith('.html')) {
-                            element.innerHTML = data;
-                        }
-                        element.removeAttribute('insert-url');
-                    }, 'text').fail(function (err) {
-                        console.error(err.status + ' ' + err.statusText)
-                        element.innerHTML = 'Sorry, there was a problem downloading the content.';
-                    });
-                } catch{ }
-            }
-        });
+
+$(document).ready(function () {
+    $('.js-insert').each(function (index, element) {
+        var url = element.getAttribute('insert-url');
+        if (url) {
+            insert_html(element, url);
+        }
     });
-})();
+});
+
+function insert_html(element, url) {
+    $.get(url, function (data) {
+        if (url.endsWith('.md')) {
+            $(element).html(marked(data));
+        }
+        if (url.endsWith('.html')) {
+            $(element).html(data);
+        }
+        element.removeAttribute('insert-url');
+    }, 'text').fail(function (err) {
+        console.error(err.status + ' ' + err.statusText)
+        $(element).html('Sorry, there was a problem downloading the content.');
+    });
+}
+
+function switch_loaded(url) {
+    $('.js-insert').html('');
+    $('#viewer').html('');
+    if (url)
+        insert_html(document.getElementById('viewer'), url);
+}
