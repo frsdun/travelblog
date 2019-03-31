30b4a9024b70e2c972a8103b5e225323e874c6d0
Duncan <frsdun@gmail.com>
2019-03-31 15:01:27 +0200

    Refactor

--- build.js
+++ build.js
@@ -1,125 +1,134 @@
 const fs = require('fs');
 const path = require('path');
-const common_mark = require('commonmark');
 
-(function () {
-    // Get the two file paths
-    if (!process.argv[2] || !process.argv[3] || !process.argv[4]) { return console.log('Error, setting input and output paths'); }
-    const input_path = path.join(__dirname, process.argv[2]);
-    const output_path = path.join(__dirname, process.argv[3]);
-    const layout_path = path.join(__dirname, process.argv[4]);
+if (!process.argv[2] || !process.argv[3] || !process.argv[4]) { return console.log('Error, setting input and output paths'); }
+const input_path = path.join(__dirname, process.argv[2]);
+const output_path = path.join(__dirname, process.argv[3]);
+const layout_path = path.join(__dirname, process.argv[4]);
 
-    delete_html_from_directory(output_path);
+delete_html_from_directory(output_path);
 
-    // Get all files in dir which do not start with _ and are either .html or .md
-    const file_names = fs.readdirSync(input_path).filter(name => !/^_/.test(name) && /\.md$|\.html$/.test(name));
+// Get all ".html" files in directory(input_path) which do not start with "_"
+const file_names = fs.readdirSync(input_path).filter(name => !/^_/.test(name) && /\.html$/.test(name));
 
-    file_names.forEach(name => {
-        // Read data out of file as a string
-        let file_data = fs.readFileSync(path.join(input_path, name), { encoding: 'utf8' });
+// Process each file
+file_names.forEach(name => {
+    // Read data out of file as a string
+    let file_data = fs.readFileSync(path.join(input_path, name), { encoding: 'utf8' });
 
-        if (/\.md$/.test(name)) {
-            // If its a markdown file convert it to html
-            file_data = convert_md_to_html(file_data);
-            name = name.replace('.md', '.html');
+    // Insert the file_data into the layout
+    const layout_data = fs.readFileSync(layout_path, { encoding: 'utf8' });
+    file_data = layout_data.replace('@link{{"src":"main-content"}}', file_data);
+
+    // Process each file
+    const final_data = process_data(file_data);
+
+    // Write file to output directory
+    fs.writeFileSync(path.join(output_path, name), final_data, { encoding: 'utf8' });
+});
+
+console.log('Build done!')
+
+
+/* -------------------------------------- Functions --------------------------------------*/
+
+
+function process_data(html) {
+    // links - link to html file, can pass in props
+    // props - json to pass up 
+    // insert - placeholder and key to insert a property value
+
+    // fist find link, then for each link resolve its inserts
+    let links = find_tokens(html, 'link');
+    links.forEach(token => {
+        if (!token.hasOwnProperty('src')) return console.log('Links must have a "src" property')
+
+        // Get the html to link in
+        let new_html = fs.readFileSync(path.join(input_path, token.src), { encoding: 'utf8' });
+
+        new_html = process_inserts(new_html, token);
+
+        // If the new html has another insert_point then process that first
+        if (new_html.includes('@link{{')) {
+            new_html = process_data(new_html);
         }
 
-        file_data = insert_into_layout(file_data, layout_path);
-
-        const final_data = process_html(file_data);
-
-        fs.writeFileSync(path.join(output_path, name), final_data, { encoding: 'utf8' });
+        html = insert_html(html, new_html, token.start_index, token.size);
     });
 
-    console.log('Build done!')
-
-
-    function insert_into_layout(html, layout_path) {
-        let layout_data = fs.readFileSync(layout_path, { encoding: 'utf8' });
-        return layout_data.replace('${{body}}', html);
-    }
-
-
-
-    function process_html(html) {
-        // Find and process any html insert points "@{{...}}"
-        const found_insert_points = find_insert_points(html).reverse();
-        //console.log(found_insert_points);
-
-        found_insert_points.forEach(insert_point => {
-            // Get the html to insert
-            let new_html = fs.readFileSync(path.join(input_path, insert_point.content.src), { encoding: 'utf8' });
-
-            // Find and process any data_points "${{...}}"
-            const found_data_points = find_data_points(new_html).reverse();
-            //console.log(found_data_points);
-
-            found_data_points.forEach(data_point => {
-                // Extract the data from the insert_point json using the key from the data_point
-                let data_content = insert_point.content[data_point.key];
-
-                if (!data_content) {
-                    console.log('Problem matching key(' + data_point.key + ') with data_point');
-                    data_content = '';
-                }
-
-                // Insert the new html into the old html and remove the placeholder
-                new_html = insert_html(new_html, data_content, data_point.start_index, data_point.size);
-            });
-
-            // If the new html has another insert_point then process that first
-            if (new_html.includes('@{{')) {
-                new_html = process_html(new_html);
+    // Second process Props
+    let props = find_tokens(html, 'props');
+    let stored_props = {};
+    props.forEach(token => {
+        for (var propertyName in token) {
+            if (propertyName != 'type' && propertyName != 'size' && propertyName != 'start_index') {
+                // stored_props.push({ key: propertyName, value: token[propertyName] })
+                stored_props[propertyName] = token[propertyName];
             }
-
-            // Insert the new html into the old html and remove the placeholder
-            html = insert_html(html, new_html, insert_point.start_index, insert_point.size);
-        });
-
-        html = set_title(html);
-
-        return html;
-    }
-
-    function set_title(html) {
-        return html;
-    }
-
-    function find_data_points(html) {
-        const pattern = /\${{([\s\S]*?)}}/g;
-        const found = [];
-        while ((match = pattern.exec(html)) != null) {
-            found.push({ 'key': match[0].replace('${{', '').replace('}}', ''), 'start_index': match.index, 'size': match[0].length })
         }
-        return found;
-    }
+        // Remove the props text
+        html = insert_html(html, '', token.start_index, token.size);
+    });
 
-    function find_insert_points(html) {
-        const pattern = /@{{([\s\S]*?)}}/g;
-        const found = [];
-        while ((match = pattern.exec(html)) != null) {
-            const content = match[0].replace(/&quot;/g, '"').replace('@{{', '{').replace('}}', '}').replace(/\r?\n|\r/g, '');
-            found.push({ 'content': JSON.parse(content), 'start_index': match.index, 'size': match[0].length })
+    // Third, place the stored_props into the matching inserts
+    html = process_inserts(html, stored_props)
+
+    return html;
+}
+
+function process_inserts(html, obj_with_prop) {
+    let inserts = find_tokens(html, 'insert');
+    inserts.forEach(token => {
+        if (!token.key) return console.log('Key not set on insert');
+        if (obj_with_prop) {
+            html = insert_html(html, obj_with_prop[token.key], token.start_index, token.size);
         }
-        return found;
-    }
+    });
+    return html;
+}
 
-    function insert_html(old_html, new_html, insert_start_position, place_holder_size) {
-        return old_html.slice(0, insert_start_position) + new_html + old_html.slice(insert_start_position + place_holder_size, old_html.length);
-    }
+function find_tokens(html, token) {
+    const pattern = new RegExp('@' + token + '{{([\\s\\S]*?)}}', 'g');
+    const found = [];
 
-    function delete_html_from_directory(directory) {
-        const file_names = fs.readdirSync(directory);
-        file_names.forEach(name => {
-            if (/\.html$/.test(name))
-                fs.unlinkSync(path.join(directory, name));
-        });
+    if (token === 'insert') {
+        while ((match = pattern.exec(html)) != null) {
+            let found_item = {};
+            found_item.key = match[0].replace('@' + token + '{{', '').replace('}}', '');
+            found_item.type = token;
+            found_item.start_index = match.index;
+            found_item.size = match[0].length;
+            found.push(found_item)
+        }
+    } else {
+        while ((match = pattern.exec(html)) != null) {
+            const content = match[0].replace('@' + token + '{{', '{').replace('}}', '}').replace(/\r?\n|\r/g, '');
+            let found_item = JSON.parse(content);
+            found_item.type = token;
+            found_item.start_index = match.index;
+            found_item.size = match[0].length;
+            found.push(found_item)
+        }
     }
+    //console.log(found.reverse());
+    return found.reverse();
+}
 
-    function convert_md_to_html(md_string) {
-        let reader = new common_mark.Parser();
-        let writer = new common_mark.HtmlRenderer();
-        let parsed = reader.parse(md_string);
-        return writer.render(parsed);
-    }
-})();
+function insert_html(old_html, new_html, insert_start_position, place_holder_size) {
+    return old_html.slice(0, insert_start_position) + new_html + old_html.slice(insert_start_position + place_holder_size, old_html.length);
+}
+
+
+
+
+
+
+
+
+function delete_html_from_directory(directory) {
+    const file_names = fs.readdirSync(directory);
+    file_names.forEach(name => {
+        if (/\.html$/.test(name))
+            fs.unlinkSync(path.join(directory, name));
+    });
+}
---
+++ build.old.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
+
+(function () {
+    // Get the two file paths
+    if (!process.argv[2] || !process.argv[3] || !process.argv[4]) { return console.log('Error, setting input and output paths'); }
+    const input_path = path.join(__dirname, process.argv[2]);
+    const output_path = path.join(__dirname, process.argv[3]);
+    const layout_path = path.join(__dirname, process.argv[4]);
+
+    delete_html_from_directory(output_path);
+
+    // Get all ".html" files in dir which do not start with "_"
+    const file_names = fs.readdirSync(input_path).filter(name => !/^_/.test(name) && /\.html$/.test(name));
+
+    file_names.forEach(name => {
+        // Read data out of file as a string
+        let file_data = fs.readFileSync(path.join(input_path, name), { encoding: 'utf8' });
+
+        file_data = insert_into_layout(file_data, layout_path);
+
+        const final_data = process_html(file_data);
+
+        fs.writeFileSync(path.join(output_path, name), final_data, { encoding: 'utf8' });
+    });
+
+    console.log('Build done!')
+
+
+    function insert_into_layout(html, layout_path) {
+        let layout_data = fs.readFileSync(layout_path, { encoding: 'utf8' });
+        return layout_data.replace('${{body}}', html);
+    }
+
+
+
+    function process_html(html) {
+        // Find and process any html insert points "@{{...}}"
+        const found_insert_points = find_insert_points(html).reverse();
+        //console.log(found_insert_points);
+
+        found_insert_points.forEach(insert_point => {
+            console.log(insert_point)
+            // First pass, fill in any @'s with "src"
+            if (insert_point.content.hasOwnProperty('src')) {
+                // Get the html to insert
+                let new_html = fs.readFileSync(path.join(input_path, insert_point.content.src), { encoding: 'utf8' });
+
+                // Find and process any data_points "${{...}}"
+                const found_data_points = find_data_points(new_html).reverse();
+                //console.log(found_data_points);
+
+                found_data_points.forEach(data_point => {
+                    // Extract the data from the insert_point json using the key from the data_point
+                    let data_content = insert_point.content[data_point.key];
+
+                    if (!data_content) {
+                        console.log('Problem matching key(' + data_point.key + ') with data_point');
+                        data_content = '';
+                    }
+
+                    // Insert the new html into the old html and remove the placeholder
+                    new_html = insert_html(new_html, data_content, data_point.start_index, data_point.size);
+                });
+
+                // If the new html has another insert_point then process that first
+                if (new_html.includes('@{{')) {
+                    new_html = process_html(new_html);
+                }
+
+                // Insert the new html into the old html and remove the placeholder
+                html = insert_html(html, new_html, insert_point.start_index, insert_point.size);
+            }
+        });
+
+        html = set_title(html);
+
+        return html;
+    }
+
+    function set_title(html) {
+        return html;
+    }
+
+    function find_data_points(html) {
+        const pattern = /\${{([\s\S]*?)}}/g;
+        const found = [];
+        while ((match = pattern.exec(html)) != null) {
+            found.push({ 'key': match[0].replace('${{', '').replace('}}', ''), 'start_index': match.index, 'size': match[0].length })
+        }
+        return found;
+    }
+
+    function find_insert_points(html) {
+        const pattern = /@{{([\s\S]*?)}}/g;
+        const found = [];
+        while ((match = pattern.exec(html)) != null) {
+            const content = match[0].replace(/&quot;/g, '"').replace('@{{', '{').replace('}}', '}').replace(/\r?\n|\r/g, '');
+            found.push({ 'content': JSON.parse(content), 'start_index': match.index, 'size': match[0].length })
+        }
+        return found;
+    }
+
+    function insert_html(old_html, new_html, insert_start_position, place_holder_size) {
+        return old_html.slice(0, insert_start_position) + new_html + old_html.slice(insert_start_position + place_holder_size, old_html.length);
+    }
+
+    function delete_html_from_directory(directory) {
+        const file_names = fs.readdirSync(directory);
+        file_names.forEach(name => {
+            if (/\.html$/.test(name))
+                fs.unlinkSync(path.join(directory, name));
+        });
+    }
+})();
